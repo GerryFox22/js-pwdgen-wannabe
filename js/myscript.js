ff73313429705e46f6d6db769a6f141c26548127
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-08-04 15:49:20 +0200

    formattazione e termine esercizio

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+
+var nome = prompt ("Inserisci il tuo nome")
+var cognome = prompt ("Inserirsci il tuo cognome")
+var colorePreferito = prompt ("Inserirsci il tuo colore preferito")
+var numero = 21;
+
+var password = nome + cognome + colorePreferito + numero
+
+document.getElementById ("password").innerHTML = password
