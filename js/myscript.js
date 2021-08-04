456cec030f7f1478208da7c9b468e94f14f857ca
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-08-04 16:01:17 +0200

    aggiunto anno di nascita

--- js/myscript.js
+++ js/myscript.js
@@ -2,8 +2,8 @@
 var nome = prompt ("Inserisci il tuo nome")
 var cognome = prompt ("Inserirsci il tuo cognome")
 var colorePreferito = prompt ("Inserirsci il tuo colore preferito")
-var numero = 21;
+var annoDiNascita = prompt ("Inserisci l'anno di nascita")
 
-var password = nome + cognome + colorePreferito + numero
+var password = nome + cognome + colorePreferito + annoDiNascita
 
 document.getElementById ("password").innerHTML = password
