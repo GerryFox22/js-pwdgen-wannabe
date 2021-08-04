bfeb882ec69b4db8ab45d5c347696ad30aee2df0
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-08-04 16:24:56 +0200

    aggiunta numero casuale finale password

--- js/myscript.js
+++ js/myscript.js
@@ -2,8 +2,10 @@
 var nome = prompt ("Inserisci il tuo nome")
 var cognome = prompt ("Inserirsci il tuo cognome")
 var colorePreferito = prompt ("Inserirsci il tuo colore preferito")
-var annoDiNascita = prompt ("Inserisci l'anno di nascita")
+var numeroScelto = parseInt( prompt ("Inserisci un numero casuale"))
+var numeroCasuale = parseInt (Math.random () * numeroScelto )
 
-var password = nome + cognome + colorePreferito + annoDiNascita
+
+var password = nome + cognome + colorePreferito + numeroCasuale
 
 document.getElementById ("password").innerHTML = password
